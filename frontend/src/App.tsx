7e193c9a7d91dd7573f6bc1f9edbc90fb928e88e
Matija Osrečki <matija.osrecki@gmail.com>
2023-05-06 13:33:09 +0200

    Define frontend starter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,73 @@
+import { ChangeEvent, useState } from 'react'
+
 function App() {
+	const [message, setMessage] = useState('')
+	const [transactionId, setTransactionId] = useState('')
+	const [currentMessage, setCurrentMessage] = useState('')
+
+	const connectWallet = () => {
+		// implement code
+	}
+
+	const handleMessageChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setMessage(e.target.value)
+	}
+
+	const submitMessage = () => {
+		// submit transaction
+	}
+
+	const handleTransactionChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setTransactionId(e.target.value)
+	}
+
+	const retrieveMessage = () => {
+		// submit transaction
+	}
+
 	return (
-		<div className="flex justify-center items-center min-h-screen   text-orange-500">
-			<h1 className="text-3xl">Hello Stacks</h1>
+		<div className="flex flex-col justify-center items-center h-screen gap-8">
+			<button
+				className="p-4 bg-indigo-500 rounded text-white"
+				onClick={connectWallet}
+			>
+				Connect Wallet
+			</button>
+			<h1 className="text-6xl font-black">Hello Stacks</h1>
+
+			<div className="grid grid-cols-3 gap-4">
+				<input
+					className="p-4 border border-indigo-500 rounded col-span-2"
+					placeholder="Write message here..."
+					onChange={handleMessageChange}
+					value={message}
+				/>
+				<button
+					className="p-4 bg-indigo-500 rounded text-white"
+					onClick={submitMessage}
+				>
+					Submit New Message
+				</button>
+
+				<input
+					className="p-4 border border-indigo-500 rounded col-span-2"
+					placeholder="Paste transaction ID to look up message"
+					onChange={handleTransactionChange}
+					value={transactionId}
+				/>
+				<button
+					className="p-4 bg-indigo-500 rounded text-white"
+					onClick={retrieveMessage}
+				>
+					Retrieve Message
+				</button>
+			</div>
+
+			{currentMessage.length > 0 ? (
+				<p className="text-2xl">{currentMessage}</p>
+			) : (
+				''
+			)}
 		</div>
 	)
 }
