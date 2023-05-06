3f56cafbf026cadc44567ca59f75e60fed26f628
Matija Osrečki <matija.osrecki@gmail.com>
2023-05-06 11:52:09 +0200

    Add ts & tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="flex justify-center items-center min-h-screen   text-orange-500">
+			<h1 className="text-3xl">Hello Stacks</h1>
+		</div>
+	)
+}
+
+export default App
