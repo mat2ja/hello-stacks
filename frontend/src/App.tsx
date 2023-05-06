dabb19be664b51d44eda3fc4b3610b94d50c7bd2
Matija Osrečki <matija.osrecki@gmail.com>
2023-05-06 16:08:42 +0200

    Connect with wallet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,56 @@
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
+import {
+	AppConfig,
+	UserSession,
+	AuthDetails,
+	showConnect,
+	UserData,
+} from '@stacks/connect'
 
 function App() {
+	const appConfig = new AppConfig(['store_write'])
+	const userSession = new UserSession({ appConfig })
+	const appDetails = {
+		name: 'Hello Stacks',
+		icon: 'https://freesvg.org/img/1541103084.png',
+	}
+
+	const [userData, setUserData] = useState<UserData | undefined>(undefined)
 	const [message, setMessage] = useState('')
 	const [transactionId, setTransactionId] = useState('')
 	const [currentMessage, setCurrentMessage] = useState('')
 
+	const reloadPage = () => {
+		window.location.reload()
+	}
+
+	useEffect(() => {
+		if (userSession.isSignInPending()) {
+			userSession.handlePendingSignIn().then((userData) => {
+				setUserData(userData)
+			})
+		} else if (userSession.isUserSignedIn()) {
+			setUserData(userSession.loadUserData())
+		}
+	}, [])
+
+	useEffect(() => {
+		if (userData) {
+			console.log('userData', userData)
+		}
+	}, [userData])
+
 	const connectWallet = () => {
-		// implement code
+		showConnect({
+			appDetails,
+			userSession,
+			onFinish: reloadPage,
+		})
+	}
+
+	const disconnectWallet = () => {
+		userSession.signUserOut()
+		reloadPage()
 	}
 
 	const handleMessageChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -27,41 +71,53 @@ function App() {
 
 	return (
 		<div className="flex flex-col justify-center items-center h-screen gap-8">
-			<button
-				className="p-4 bg-indigo-500 rounded text-white"
-				onClick={connectWallet}
-			>
-				Connect Wallet
-			</button>
+			{!userSession.isUserSignedIn() ? (
+				<button
+					className="p-4 bg-indigo-500 rounded text-white"
+					onClick={connectWallet}
+				>
+					Connect Wallet
+				</button>
+			) : (
+				<button
+					className="p-4 bg-indigo-200 rounded text-indigo-800"
+					onClick={disconnectWallet}
+				>
+					Sign out
+				</button>
+			)}
+
 			<h1 className="text-6xl font-black">Hello Stacks</h1>
 
-			<div className="grid grid-cols-3 gap-4">
-				<input
-					className="p-4 border border-indigo-500 rounded col-span-2"
-					placeholder="Write message here..."
-					onChange={handleMessageChange}
-					value={message}
-				/>
-				<button
-					className="p-4 bg-indigo-500 rounded text-white"
-					onClick={submitMessage}
-				>
-					Submit New Message
-				</button>
+			{userData && (
+				<div className="grid grid-cols-3 gap-4">
+					<input
+						className="p-4 border border-indigo-500 rounded col-span-2"
+						placeholder="Write message here..."
+						onChange={handleMessageChange}
+						value={message}
+					/>
+					<button
+						className="p-4 bg-indigo-500 rounded text-white"
+						onClick={submitMessage}
+					>
+						Submit New Message
+					</button>
 
-				<input
-					className="p-4 border border-indigo-500 rounded col-span-2"
-					placeholder="Paste transaction ID to look up message"
-					onChange={handleTransactionChange}
-					value={transactionId}
-				/>
-				<button
-					className="p-4 bg-indigo-500 rounded text-white"
-					onClick={retrieveMessage}
-				>
-					Retrieve Message
-				</button>
-			</div>
+					<input
+						className="p-4 border border-indigo-500 rounded col-span-2"
+						placeholder="Paste transaction ID to look up message"
+						onChange={handleTransactionChange}
+						value={transactionId}
+					/>
+					<button
+						className="p-4 bg-indigo-500 rounded text-white"
+						onClick={retrieveMessage}
+					>
+						Retrieve Message
+					</button>
+				</div>
+			)}
 
 			{currentMessage.length > 0 ? (
 				<p className="text-2xl">{currentMessage}</p>
